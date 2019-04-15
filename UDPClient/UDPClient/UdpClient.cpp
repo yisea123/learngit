6d7f9caff1fa1f4c207c85e78eb872bcfd32024d
xiaohuohu <467572873@qq.com>
2019-04-15 15:53:24 +0800

    UDP

--- UDPClient/UDPClient/UdpClient.cpp
+++ UDPClient/UDPClient/UdpClient.cpp
@@ -1,4 +1,7 @@
-#include <WinSock2.h>
+ 
+//UdpNetClient.cpp
+ 
+#include <Winsock2.h>
 #include <stdio.h>
  
 void main()
@@ -8,34 +11,58 @@ void main()
 	WSADATA wsaData;
 	int err;
  
-	wVersionRequested = MAKEWORD(1,1);//高字节,低字节
+	wVersionRequested = MAKEWORD(1,1);
  
 	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0)
+	if(err != 0)
 	{
-		return;//加载套接字失败
+		return;
 	}
  
-	if (LOBYTE(wsaData.wVersion)!=1 || HIBYTE(wsaData.wVersion)!=1)
+	if(LOBYTE(wsaData.wVersion) != 1 ||     //低字节为主版本
+		HIBYTE(wsaData.wVersion) != 1)      //高字节为副版本
 	{
 		WSACleanup();
-		return;//版本不支持
+		return;
 	}
  
-	//创建套接字
-	SOCKET sockClient = socket(AF_INET, SOCK_DGRAM, 0);
+	printf("Client is operating!\n\n");
+	//创建用于监听的套接字
+	SOCKET sockSrv = socket(AF_INET,SOCK_DGRAM,0);
  
-	SOCKADDR_IN addrSrv;
+	sockaddr_in  addrSrv;
 	addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	addrSrv.sin_family = AF_INET;
-	addrSrv.sin_port = htons(6000);//host2net 16位
+	addrSrv.sin_port = htons(6000);
+	
+	
+	int len = sizeof(SOCKADDR);
  
-	//发送数据
-	sendto(sockClient, "Hello!", strlen("Hello!")+1, 0, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
+	char recvBuf[100];    //收
+	char sendBuf[100];    //发
+	char tempBuf[100];    //存储中间信息数据
+			
+	while(1)
+	{
+		
+		printf("Please input data: \n");
+		gets(sendBuf);
+		sendto(sockSrv,sendBuf,strlen(sendBuf)+1,0,(SOCKADDR*)&addrSrv,len);
+		//等待并数据
+		recvfrom(sockSrv,recvBuf,100,0,(SOCKADDR*)&addrSrv,&len);
  
-	//关闭套接字
-	closesocket(sockClient);
-	WSACleanup();
-	system("Pause");
+		if('q' == recvBuf[0])
+		{
+			sendto(sockSrv,"q",strlen("q")+1,0,(SOCKADDR*)&addrSrv,len);
+			printf("Chat end!\n");
+			break;
+		}
+		sprintf_s(tempBuf,"%s say : %s",inet_ntoa(addrSrv.sin_addr),recvBuf);
+		printf("%s\n",tempBuf);
  
+		//发送数据
+		
+	}
+		closesocket(sockSrv);
+		WSACleanup();
 }
--- UDPSrv/UDPSrv/UdpSrv.cpp
+++ UDPSrv/UDPSrv/UdpSrv.cpp
@@ -1,49 +1,69 @@
-#include <winsock2.h>
+//UdpNetSrv.cpp
+ 
+#include <Winsock2.h>
 #include <stdio.h>
-
+ 
 void main()
 {
 	//加载套接字库
-	WORD wVrsionRequested;
+	WORD wVersionRequested;
 	WSADATA wsaData;
 	int err;
-
-	wVrsionRequested = MAKEWORD(1,1);
-	err = WSAStartup( wVrsionRequested,&wsaData);//加载库返回版本信息
-
-	if(err!=0)
+ 
+	wVersionRequested = MAKEWORD(1,1);
+ 
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if(err != 0)
 	{
 		return;
 	}
-
-	if(LOBYTE(wsaData.wVersion)!=1 || HIBYTE(wsaData.wVersion)!=1)
+ 
+	if(LOBYTE(wsaData.wVersion) != 1 ||     //低字节为主版本
+		HIBYTE(wsaData.wVersion) != 1)      //高字节为副版本
 	{
-		WSACleanup();//终止对套接字的使用
+		WSACleanup();
 		return;
-
 	}
+ 
+	printf("server is operating!\n\n");
 	//创建用于监听的套接字
-	SOCKET sockSrv=socket(AF_INET,SOCK_DGRAM,0);//0自动选择合适的协议
-
-	SOCKADDR_IN addrSrv;//除了sa_family全要按网络字节顺序排序
-	addrSrv.sin_addr.S_un.S_addr=htonl(INADDR_ANY);
-	addrSrv.sin_family=AF_INET;
-	addrSrv.sin_port=htons(6000);//1024以上的端口
-
-	//绑定套接字
-	bind(sockSrv,(SOCKADDR*)&addrSrv,sizeof(SOCKADDR));//第三个参数地址结构大小
-
-	//等到并接受数据
-	SOCKADDR_IN addrClient;
-	int len=sizeof(SOCKADDR);
-	char recvBuf[100];
-	while(1)//无限接收
+	SOCKET sockSrv = socket(AF_INET,SOCK_DGRAM,0);
+ 
+	SOCKADDR_IN addrSrv;     //定义sockSrv发送和接收数据包的地址
+	addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons(6000);
+	
+	//绑定套接字, 绑定到端口
+	bind(sockSrv,(SOCKADDR*)&addrSrv,sizeof(SOCKADDR));
+	//将套接字设为监听模式， 准备接收客户请求
+	
+ 
+	SOCKADDR_IN addrClient;   //用来接收客户端的地址信息
+	int len = sizeof(SOCKADDR);
+	char recvBuf[100];    //收
+	char sendBuf[100];    //发
+	char tempBuf[100];    //存储中间信息数据
+			
+	while(1)
 	{
+		//等待并数据
 		recvfrom(sockSrv,recvBuf,100,0,(SOCKADDR*)&addrClient,&len);
-		printf("%s\n",recvBuf);
+		if('q' == recvBuf[0])
+		{
+			sendto(sockSrv,"q",strlen("q")+1,0,(SOCKADDR*)&addrClient,len);
+			printf("Chat end!\n");
+			break;
+		}
+		sprintf_s(tempBuf,"%s say : %s",inet_ntoa(addrClient.sin_addr),recvBuf);
+		printf("%s\n",tempBuf);
+ 
+		//发送数据
+		printf("Please input data: \n");
+		gets(sendBuf);
+		sendto(sockSrv,sendBuf,strlen(sendBuf)+1,0,(SOCKADDR*)&addrClient,len);
 	}
-	closesocket(sockSrv);
-	WSACleanup();
-	system("Pause");
-
+		closesocket(sockSrv);
+		WSACleanup();
 }
+ 
